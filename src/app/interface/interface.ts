9b498498ac1bbefa4973b17a4d31b6a3898d62fe
Mahir Durna <durna.m@outlook.de>
2026-05-25 00:29:51 +0200

    feat: add survey service with signal and poll interface

---
+++ src/app/interface/interface.ts
@@ -0,0 +1,26 @@
+export interface Option {
+  id: number;
+  option: string;
+  vote: number;
+}
+
+export interface Question {
+  id: number;
+  question: string;
+  options: Option[];
+}
+
+export interface Categories {
+  id: number;
+  category: string;
+}
+
+export interface Poll {
+  id: number;
+  title: string;
+  description: string;
+  endDate: string;
+  categoryId: number;
+  categories: Categories;
+  pollQuestion: Question[];
+}
--- src/app/services/survey.ts
+++ src/app/services/survey.ts
@@ -1,13 +1,17 @@
-import {inject, Injectable} from '@angular/core';
+import {inject, Injectable, signal } from '@angular/core';
 import {SupabaseService} from './supabase';
+import { Poll } from '../interface/interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SurveyService {
-  supabaseService = inject(SupabaseService)
+  supabaseService = inject(SupabaseService);
 
-  async getAllSurvesy(){
-    return await this.supabaseService.getClient().from('surveys').select('*')
-  };
+  surveys = signal<Poll[]>([])
+
+  async getAllSurvey() {
+    const { data } = await this.supabaseService.getClient().from('surveys').select('*');
+    this.surveys.set(data ?? [])
+  }
 }
